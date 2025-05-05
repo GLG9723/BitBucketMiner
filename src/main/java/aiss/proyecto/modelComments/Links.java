7469b369c41a9bc58aea177e40cdf9597a9a6abf
Julio Macias <julmacgon@alum.us.es>
2025-05-05 14:32:38 +0200

    unused import eliminados

--- src/main/java/aiss/proyecto/modelComments/Links.java
+++ src/main/java/aiss/proyecto/modelComments/Links.java
@@ -2,9 +2,7 @@
 package aiss.proyecto.modelComments;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Links {
